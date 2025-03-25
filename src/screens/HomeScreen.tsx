57326784018c75e1c5baa58fb68ac21a28129457
mbanovic20 <mbanovicb@gmail.com>
2025-03-25 14:13:12 +0100

    home screen napravljen

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+// src/screens/HomeScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-lg font-bold">Home Screen</Text>
+    </View>
+  );
+}
