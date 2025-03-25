8ba78058bb95e73191132b59e139ba64a1a1e528
mbanovic20 <mbanovicb@gmail.com>
2025-03-25 13:58:57 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./App.tsx", "./src/**/*.{ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
+  
