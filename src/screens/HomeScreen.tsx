b28c14c20f51f53344c47810b2e35dc7dc1e9355
mbanovic20 <mbanovicb@gmail.com>
2025-03-25 14:21:37 +0100

    Testni screen za profile

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,3 @@
-// src/screens/HomeScreen.tsx
 import React from 'react';
 import { View, Text } from 'react-native';
 
---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-lg font-bold">Profile Screen</Text>
+    </View>
+  );
+}
